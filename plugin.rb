558b898dfecea578512385e0c542f511cdac4a74
Oliver530 <oli.gerhardt@gmail.com>
2025-02-14 18:04:23 +0100

    Refined ruby implementation 2

--- plugin.rb
+++ plugin.rb
@@ -1,55 +1,80 @@
-# frozen_string_literal: true
-
 # name: discourse-rumx-utm
-# about: Adds "rumx" UTM parameters to all outgoing links.
+# about: Adds "rumx" UTM parameters to all outgoing external links in posts
 # version: 1.0.0
 # authors: Oliver Gerhardt
 # url: https://github.com/Oliver530/discourse-rumx-utm
 
-enabled_site_setting :utm_rumx_enabled
-
 after_initialize do
-  # Hook into the cooked post processing
-  on(:post_process_cooked) do |doc, post|
-    # Only process if the plugin is enabled via Site Settings.
-    next unless SiteSetting.utm_rumx_enabled
-
-    # Determine the internal host from the Discourse base URL.
-    begin
-      internal_host = URI.parse(Discourse.base_url).host
-    rescue StandardError
-      internal_host = nil
+    module ::DiscourseRUMXUTM
+      class Engine < ::Rails::Engine
+        engine_name "discourse_rumx_utm"
+        isolate_namespace DiscourseRUMXUTM
+      end
+  
+      # This processor will be applied to cooked posts.
+      class UTMProcessor
+        require 'uri'
+        require 'rack/utils'
+  
+        def self.process(doc)
+          # Iterate through each link in the cooked post.
+          doc.css("a").each do |link|
+            href = link.get_attribute("href")
+            next if href.blank?
+  
+            if external_link?(href)
+              new_href = add_utm_params(href)
+              link.set_attribute("href", new_href)
+            end
+          end
+        end
+  
+        # Determine if the link is external by comparing hosts.
+        def self.external_link?(href)
+          begin
+            uri = URI.parse(href)
+          rescue URI::InvalidURIError
+            return false
+          end
+  
+          # If the URI is relative or missing a host, consider it internal.
+          return false if uri.host.blank?
+  
+          # Compare with the current site's host.
+          begin
+            current_host = URI.parse(Discourse.base_url).host
+          rescue
+            current_host = ""
+          end
+  
+          uri.host != current_host
+        end
+  
+        # Append UTM parameters to the URL.
+        def self.add_utm_params(url)
+          begin
+            uri = URI.parse(url)
+          rescue URI::InvalidURIError
+            return url
+          end
+  
+          # Parse existing query parameters.
+          params = Rack::Utils.parse_nested_query(uri.query)
+          # Add/override UTM parameters.
+          params.merge!(
+            "utm_source"   => "rumx",
+            "utm_medium"   => "referral",
+            "utm_campaign" => "rumx-forum"
+          )
+          # Build the updated query string.
+          uri.query = Rack::Utils.build_nested_query(params)
+          uri.to_s
+        end
+      end
     end
-
-    # Find all anchor tags with an href attribute.
-    doc.css("a[href]").each do |anchor|
-      href = anchor["href"]
-      begin
-        uri = URI.parse(href)
-      rescue URI::InvalidURIError
-        # Skip if the URL cannot be parsed.
-        next
-      end
-
-      # Only process absolute URLs (i.e. those with a scheme and host).
-      next unless uri.scheme && uri.host
-
-      # Skip internal links.
-      if internal_host && uri.host.downcase == internal_host.downcase
-        next
-      end
-
-      # Parse the current query string, if any.
-      query_params = URI.decode_www_form(uri.query || "")
-
-      # Append UTM parameters if they aren’t already present.
-      query_params << ["utm_source", "rumx"]   unless query_params.any? { |k, _| k == "utm_source" }
-      query_params << ["utm_medium", "rumx"]   unless query_params.any? { |k, _| k == "utm_medium" }
-      query_params << ["utm_campaign", "rumx"] unless query_params.any? { |k, _| k == "utm_campaign" }
-
-      # Set the updated query string.
-      uri.query = URI.encode_www_form(query_params)
-      anchor["href"] = uri.to_s
+  
+    # Hook into the cooked post processing step.
+    on(:post_process_cooked) do |doc, post|
+      ::DiscourseRUMXUTM::UTMProcessor.process(doc)
     end
   end
-end
